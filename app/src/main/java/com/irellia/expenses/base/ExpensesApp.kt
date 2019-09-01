3bcfad71c7829d3fd0a155a2debbd891f2efb70b
MostafaEsmail <mesmail@sictsystems.com>
2019-09-01 15:57:58 +0200

    add Crashlytics Version 2.3.0

---
+++ app/src/main/java/com/irellia/expenses/base/ExpensesApp.kt
@@ -0,0 +1,10 @@
+package com.irellia.expenses.base
+
+import androidx.multidex.MultiDexApplication
+
+/**
+ * Created by µðšţãƒâ ™ on 9/1/2019.
+ * ->
+ */
+class ExpensesApp  : MultiDexApplication(){
+}
